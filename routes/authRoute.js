231985c98236ba6a372f7fbefbb463a96fedd83c
Abhay Rajak <mail2rajab01@gmail.com>
2023-08-11 17:38:04 +0530

    login functionality implemented

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,8 @@
 import express from "express";
-import { registerController } from "../controllers/authController.js";
+import {
+  loginController,
+  registerController,
+} from "../controllers/authController.js";
 
 //router object
 const router = express.Router();
@@ -7,5 +10,8 @@ const router = express.Router();
 //routing
 //register || post method
 router.post("/register", registerController);
+
+//login || post method
+router.post("/login", loginController);
 
 export default router;
