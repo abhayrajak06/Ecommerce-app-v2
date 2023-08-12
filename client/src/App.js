20fe2dbf73d5ae4691c421baca63886d72a7fb18
Abhay Rajak <mail2rajab01@gmail.com>
2023-08-12 12:04:39 +0530

    components setup and pages routing implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import Contact from "./pages/Contact";
+import Policy from "./pages/Policy";
+import About from "./pages/About";
+import Pagenotfound from "./pages/Pagenotfound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path={"/"} element={<HomePage />} />
+        <Route path={"/contact"} element={<Contact />} />
+        <Route path={"/policy"} element={<Policy />} />
+        <Route path={"/about"} element={<About />} />
+        <Route path={"/*"} element={<Pagenotfound />} />
+      </Routes>
+    </>
   );
 }
 
